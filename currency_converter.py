d1659572e4066fd25d9dea5862fb087cbf5e866d
Stephan Mah <sm8170@gmail.com>
2021-12-02 08:37:29 -0500

    added currency conversion method

--- currency_converter.py
+++ currency_converter.py
@@ -10,17 +10,32 @@ api = "c8659d6570fb1fa0887c064a"
 default_pair = 'https://v6.exchangerate-api.com/v6/c8659d6570fb1fa0887c064a/pair/EUR/GBP/1'
 
 response = requests.get(default_pair)
-data = response.json()
+#currency_dict = response.json()
 
-diction = {'Hello': 60}
-string_returned = json.dumps(data)
-print(string_returned)
 
-dtest = json.loads(string_returned)
-print(type(string_returned))
+#returned_string = json.dumps(currency_dict)
+#print(currency_dict)
 
-def currency_conversion():
-    return
+#dtest = json.loads(returned_string)
+#values = currency_dict.get("base_code")
+#print(type(values))
+#print(values)
+
+
+def currency_conversion(site):
+    data = requests.get(site)
+    currency_dictionary = data.json()
+
+    from_currency = currency_dictionary.get("base_code")
+    to_currency = currency_dictionary.get("target_code")
+    amount = currency_dictionary.get("conversion_result")
+
+
+    print("This is the base currency: " ,from_currency)
+    print("This is the target currency: ", to_currency)
+    print("This is the amount: " ,amount)
+
+
 
 
 
@@ -33,7 +48,7 @@ root.configure(bg = "White")
 
 heading = Label(root, text = "Realtime Currency Converter", font = ("Comic    Sans MS", 18)).place(x=100)
 
-
+currency_conversion(default_pair)
 
 
 
