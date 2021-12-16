c0a86df309ea4fd396cdd6daf392b7e9e0957542
Stephan Mah <sm8170@gmail.com>
2021-12-16 09:08:17 -0500

    object oriented tkinter

--- currency_converter.py
+++ currency_converter.py
@@ -1,26 +1,22 @@
 import tkinter as tk
-from tkinter import *
 import json
 import requests
 
 
 url = 'https://v6.exchangerate-api.com/v6/c8659d6570fb1fa0887c064a/latest/USD'
 api = "c8659d6570fb1fa0887c064a"
-
 default_pair = 'https://v6.exchangerate-api.com/v6/c8659d6570fb1fa0887c064a/pair/EUR/GBP/1'
 
-response = requests.get(default_pair)
-#currency_dict = response.json()
 
+response = requests.get(url)
+currency_dict = response.json()
 
-#returned_string = json.dumps(currency_dict)
-#print(currency_dict)
+returned_string = json.dumps(currency_dict)
 
-#dtest = json.loads(returned_string)
-#values = currency_dict.get("base_code")
-#print(type(values))
-#print(values)
+dtest = json.loads(returned_string)
+values = currency_dict.get("conversion_rates")
 
+currency_list = values.keys()
 
 def currency_conversion(site):
     data = requests.get(site)
@@ -36,21 +32,15 @@ def currency_conversion(site):
     print("This is the amount: " ,amount)
 
 
-
-
-
-root = tk.Tk()
-root.title("Currency Converter")
-root.geometry("500x500")
-root.resizable(0,0)
-root.configure(bg = "White")
-
-
-heading = Label(root, text = "Realtime Currency Converter", font = ("Comic    Sans MS", 18)).place(x=100)
-
 currency_conversion(default_pair)
 
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('500x500')
+        self.resizable(0,0)
+        self.configure(bg = "White")
 
-
-root.update()
-root.mainloop()
+if __name__ == "__main__":
+        app = App()
+        app.mainloop()
